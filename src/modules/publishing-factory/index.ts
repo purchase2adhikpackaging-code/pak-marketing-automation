9ea80d905814db9da4345f163f39e52f43cdfb23
purchase2adhikpackaging-code <purchase2adhikpackaging@gmail.com>
2026-09-16 01:48:49 +0530

    feat: export textbook visual QA

--- src/modules/publishing-factory/index.ts
+++ src/modules/publishing-factory/index.ts
@@ -23,5 +23,6 @@ export * from "./renderer";
 export * from "./repair";
 export * from "./state-machine";
 export * from "./visual-production";
+export * from "./visual-qa";
 export * from "./visual-resolver";
 export * from "./worker-queue";
